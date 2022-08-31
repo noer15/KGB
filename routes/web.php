d15b77aa1c6969505a3d00d93a4d414492b947f0
Nur Hidayat <noer4236@gmail.com>
2022-08-31 19:51:20 +0700

    feat: template login and admin

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('/home');
 });
+Route::get('/auth/login', function() {
+    return view('/auth.login');
+})->name('login');
