59ee26ee93565d63e4025a714de93fca5846fd08
Akhmad Junaedi <akhmadjunaedi@Akhmad-Macbook-Pro-KW.local>
2022-08-31 18:54:31 +0700

    Create database migrations

--- database/migrations/2022_08_31_110809_create_attendances_table.php
+++ database/migrations/2022_08_31_110809_create_attendances_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('attendances', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('check_in');
+            $table->dateTime('check_out');
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('attendances');
     }
 };
